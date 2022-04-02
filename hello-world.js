3df944ace372c0183e7b203ef2ed436db55ab6a6
Joseph Greve <limejoe517@gmail.com>
2022-04-02 16:29:11 -0400

    Initial LIRC support

--- hello-world.js
+++ hello-world.js
@@ -1,4 +1,8 @@
 var http = require('http');
+var lirc_node = require('lirc_node');
+lirc_node.init();
+console.log(lirc_node.remotes);
+
 var server = http.createServer(function (request, response) { response.writeHead(200, {"Content-Type": "text/plain"});
 response.end("Hello World!\n"); });
 server.listen(8000);
